59c67d33434442e34ba4407ca95f9788d8684e1a
EdgarB <edgar_daniel_1996@hotmail.com>
2016-04-27 17:14:07 -0500

    Player controller added

---
+++ Proyecto Final/Assets/Scripts/PlayerController.js
@@ -0,0 +1,28 @@
+﻿#pragma strict
+
+import UnityEngine;
+import System.Collections;
+
+public var fVelocity :float;
+
+private var rThisRigidbody : Rigidbody2D;
+private var fAxis : float;
+
+function Start()
+{
+	rThisRigidbody = GetComponent(Rigidbody2D);
+}
+
+
+function Update()
+{
+	fAxis =  Input.GetAxis("Horizontal");
+
+	if(fAxis != 0.0f)
+		rThisRigidbody.velocity = new Vector2 (fAxis * fVelocity, 0);
+	else
+	{
+		fAxis = Input.GetAxis("Vertical");
+		rThisRigidbody.velocity = new Vector2(0,fAxis * fVelocity);
+	}
+}
